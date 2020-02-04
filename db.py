a3f62dd2081ceabb1369972764a301e20790c652
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-02-04 01:31:02 -0800

    fix

--- db.py
+++ db.py
@@ -1,15 +1,24 @@
 import yaml
 
 class DB(object):
-	def __init__(self):
-		with open('mydb') as f:
-    		self.db = yaml.load(f, Loader=yaml.FullLoader)
+    def __init__(self):
+        with open('mydb') as f:
+            self.db = yaml.load(f, Loader=yaml.FullLoader)
 
     def save(self, usr, index, text):
-    	self.db[usr] = self.db.get(usr, {})
-    	self.db[usr][index] = text
-    	self._save()
+        self.db[usr] = self.db.get(usr, {})
+        self.db[usr][index] = text
+        self._save()
+
+    def getQuestionIndex(self, usr):
+        self.db[usr] = self.db.get(usr, {})
+        i = 0
+        while True:
+            if i in self.db[usr]:
+                i += 1
+            else:
+                return i
 
     def _save(self):
-    	with open('mydb', 'w') as f:
-    		f.write(yaml.dump(self.db, sort_keys=True, indent=2, allow_unicode=True))
+        with open('mydb', 'w') as f:
+            f.write(yaml.dump(self.db, sort_keys=True, indent=2, allow_unicode=True))
--- social.py
+++ social.py
@@ -31,53 +31,55 @@ tele = updater.bot
 debug_group = tele.get_chat(-1001198682178)
 
 def askNext(usr, msg):
-	idx = db.getQuestionIndex(usr)
-	if idx == len(questions):
-		return
-	msg.reply(questions[idx])
+    idx = db.getQuestionIndex(usr)
+    if idx == len(questions):
+        return
+    msg.reply(questions[idx])
 
 @log_on_fail(debug_group)
 def handlePrivate(update, context):
-	usr = update.effective_user
-	msg = update.effective_message
-	if not usr or not msg:
-		return
-	usr = usr.username
-	if not usr:
-		return msg.reply_text('Please Specify username before using me.')
-	os.system('mkdir photo > /dev/null 2>&1')
-	photo = None
-	profile = update.effective_user.get_profile_photos()
-	if profile.total_count > 0 and 
-		not path.exists('photo/' + usr):
-		photo = profile.photos[0]
-	if msg.photo:
-		photo = msg.photo
-	if photo:
-		photo[0].get_file().download('photo/' + usr)
-		msg.reply_text('Received/updated your photo.')
-		return askNext(usr, msg)
-	text = msg.text
-	if not text:
-		return
-	if msg.reply_to_message:
-		question = msg.reply_to_message.text
-		if question in questions:
-			index = questions.index(question)
-			db.save(usr, index, text)
-			msg.reply_text('Your answer recorded.')
-			return askNext(usr, msg)
-	idx = db.getQuestionIndex(usr)
-	if idx == None:
-		msg.reply_text(HELP)
-		return askNext(usr, msg)
-	if idx == len(questions):
-		return msg.reply_text(HELP2)
-	db.save(user, idx, text)
-	msg.reply_text('Your answer recorded.')
-	return askNext(usr, msg)
+    usr = update.effective_user
+    msg = update.effective_message
+    if not usr or not msg:
+        return
+    usr = usr.username
+    if not usr:
+        return msg.reply_text('Please Specify username before using me.')
+    os.system('mkdir photo > /dev/null 2>&1')
+    photo = None
+    profile = update.effective_user.get_profile_photos()
+    if profile.total_count > 0 and not path.exists('photo/' + usr):
+        photo = profile.photos[0]
+    if msg.photo:
+        photo = msg.photo
+    if photo:
+        photo[0].get_file().download('photo/' + usr)
+        msg.reply_text('Received/updated your photo.')
+        return askNext(usr, msg)
+    text = msg.text
+    if not text:
+        return
+    if msg.reply_to_message:
+        question = msg.reply_to_message.text
+        if question in questions:
+            index = questions.index(question)
+            db.save(usr, index, text)
+            msg.reply_text('Your answer recorded.')
+            return askNext(usr, msg)
+    idx = db.getQuestionIndex(usr)
+    if idx == None:
+        msg.reply_text(HELP)
+        return askNext(usr, msg)
+    if idx == len(questions):
+        return msg.reply_text(HELP2)
+    db.save(user, idx, text)
+    msg.reply_text('Your answer recorded.')
+    return askNext(usr, msg)
 
-
+@log_on_fail(debug_group)
+def handleCommand(update, context):
+    pass
+    
 dp = updater.dispatcher
 dp.add_handler(MessageHandler(Filters.private and (~Filters.command), handlePrivate))
 dp.add_handler(MessageHandler(Filters.private and Filters.command, handleCommand))
